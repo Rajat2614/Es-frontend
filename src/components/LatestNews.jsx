764df033628879878def80d7506342cfdc97eee9
Rajat2614 <rajjjatchauhan@gmail.com>
2023-06-08 22:27:57 +0530

    A's removed

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -19,7 +19,7 @@ const LatestNews = () => {
     return (
         <div className='container m-20 ml-10 mr-10 p-5 grid grid-cols-5 gap-2 w-[100%]'>
             <div className=" fav grid-rows w-[80%] h-[100%] ">
-                <ul className='list-none text-right'><span className='font-bold'>AMERICA'S FAVORITES</span>
+                <ul className='list-none text-right'><span className='font-bold'>AMERICAS FAVORITES</span>
                     <li>#TomBrady</li>
                     <li>#MichaelJordan</li>
                     <li>#Shaq</li>
@@ -44,7 +44,7 @@ const LatestNews = () => {
                 <NewsArticles initialData={initialData}/>
             </div>
             <div className="editor w-[80%] ">
-                <ul className='list-none text-left'><span className='font-bold'>EDITOR'S PICK</span>
+                <ul className='list-none text-left'><span className='font-bold'>EDITORS PICK</span>
 
                 </ul>
             </div>
